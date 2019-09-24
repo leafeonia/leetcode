445f679ed3f7fb4df6c4e7d2ba93059f19286908
leafeonia <maoym@126.com>
2019-09-24 20:17:02 +0800

    problem 34

--- java/src/Main.java
+++ java/src/Main.java
@@ -2,7 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.divide(200000000, -2));
+        int a[] = {6};
+        System.out.println(solution.searchRange(a, 7)[0]);
+        System.out.println(solution.searchRange(a, 7)[1]);
     }
 }
 
