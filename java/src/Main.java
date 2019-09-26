1a815b922afeb30da1a86d3aac429c4225fdcf0a
leafeonia <maoym@126.com>
2019-09-26 09:44:13 +0800

    problem 46

--- java/src/Main.java
+++ java/src/Main.java
@@ -2,9 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int a[] = {6};
-        System.out.println(solution.searchRange(a, 7)[0]);
-        System.out.println(solution.searchRange(a, 7)[1]);
+        int a[] = {1,2,3};
+        System.out.println(solution.permute(a));
     }
 }
 
