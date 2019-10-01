f3a7069974f8be532a81c05196e8012ca9393d72
leafeonia <maoym@126.com>
2019-10-01 23:53:16 +0800

    problem 54

--- java/src/Main.java
+++ java/src/Main.java
@@ -2,7 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.myPow(-1.00000, Integer.MIN_VALUE));
+        int a[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        System.out.println(solution.spiralOrder(a));
     }
 }
 
