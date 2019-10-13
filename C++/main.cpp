2c76e909c2b2dcbd4c3e26bd4bc6e09066849e77
leafeonia <maoym@126.com>
2019-10-13 14:40:19 +0800

    problem 722

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,8 +1,13 @@
 #include "solution.cpp"
 using namespace std;
 
+
 int main(){
     Solution sol;
-    cout << (double)1 << " " << double(1);
+    vector<string> source({"e/*/eabeea/*///*c*////*dc*//bcadcde/*/acbe//*d/*/*//ae//*dc//*cc//*//*eaebb*//"});
+    vector<string> ans = sol.removeComments(source);
+    for(auto line: ans) cout << line << endl;
     return 0;
 }
+//e/*/eabeea/*///*c*////*dc*//bcadcde/*/acbe//*d/*/*//ae//*dc//*cc//*//*eaebb*//
+//["/*/dadb/*/aec*////*//*ee*//*//b*////*badbda//*bbacdbbd*//ceb//*cdd//**//de*////*","ec//*//*eebd/*/*//*////*ea/*/bc*//cbdacbeadcac/*/cee*//bcdcdde*//adabeaccdd//*","ddadbede//*//*/*/ac/*/ea//*bbeb/*/ea//*a//*//*cdbeb*//ab/*/abde/*//*/d//**////*",,
